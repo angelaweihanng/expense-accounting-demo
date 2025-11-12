8a334a6753a31d12eef59ceaf2318629e9556487
angelaweihanng <ng_weihan@outlook.com>
2025-11-12 13:55:06 +0100

    edited endpoint

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const SHEET_ID = "1BxMM6j4EzXNLqlKVIlj5R90BsxJt_GC11bcM3xYm1Ok";
-export const GAS_ENDPOINT = "AKfycbxtSheelDiDt9HT_wMQpTWriZHHGjXtljIx4SH_g1T4yfszUzAgHsxjxgXh8ZTheyBM";
+export const GAS_ENDPOINT = "AKfycbzZb1gZ1TSW-FdBf7lAfczr0MbIjpxnjgIgujdNVW4v_FW1GtZa05rpI0ce2rMlmv2_";
